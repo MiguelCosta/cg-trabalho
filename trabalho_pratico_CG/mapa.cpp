653749b5d9f16a11962d500abef92aa669d475d6
rafaelcaldeirasilva <rafaelcaldeirasilva@db629c75-4b32-85d7-0afe-a27431d4d09a>
2011-04-12 17:58:53 +0000

    mapa

--- trabalho_pratico_CG/mapa.cpp
+++ trabalho_pratico_CG/mapa.cpp
@@ -1,6 +1,29 @@
 
+#include "mapa.h"
 
 
 
-class Mapa	{
-	public:
+	/* Cria aleatoriamente um mapa */
+	mapa::mapa(void)	{
+		//srand (time(NULL));
+		
+		/* Colocar arvores */
+		posicao p;
+		
+		for( int i=0 ; i < NUM_ARVORES ; i++)	{
+			p.x = rand() * 2000 / RAND_MAX;
+
+	}
+
+	/* Cria um mapa com base num ficheiro de configuração */
+	mapa(char *filename)	{
+
+	}
+	
+	//---
+	
+	/* Destroi o mapa */
+	~mapa(void)	{
+
+	}
+
