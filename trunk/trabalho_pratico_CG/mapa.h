d9f4d43a3aecbc3579f8255395ddf4d23552b13d
rafaelcaldeirasilva <rafaelcaldeirasilva@db629c75-4b32-85d7-0afe-a27431d4d09a>
2011-04-12 17:58:53 +0000

    mapa

--- trunk/trabalho_pratico_CG/mapa.h
+++ trunk/trabalho_pratico_CG/mapa.h
@@ -3,9 +3,39 @@
 #ifndef __MAPA
 #define __MAPA
 
+#define NUM_ARVORES 2000
+
 #include <list>
 
+class mapa	{
+	public:
+		
+		typedef struct _posicao	{
+			int x;
+			int y;
+		}posicao;
 
+		// Terreno
+		list<posicao> arvores;
+		list<posicao> pedras;
+
+		//Missao
+		posicao chaves[3];
+		list<posicao> torres;
+		posicao inicial;
+		posicao edificio;
+
+
+		/* Cria aleatoriamente um mapa */
+		mapa(void);
+		/* Cria um mapa com base num ficheiro de configuração */
+		mapa(char *filename);
+
+		//---
+
+		/* Destroi o mapa */
+		~mapa(void);
+}
 
 
 #endif
