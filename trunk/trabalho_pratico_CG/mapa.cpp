efa901ac03d28b765d1e35e9317b0dc9e50c303d
rafaelcaldeirasilva <rafaelcaldeirasilva@db629c75-4b32-85d7-0afe-a27431d4d09a>
2011-04-13 00:07:03 +0000

    Já cria mapas em condições!

--- trunk/trabalho_pratico_CG/mapa.cpp
+++ trunk/trabalho_pratico_CG/mapa.cpp
@@ -1,8 +1,8 @@
 
 
 #include "mapa.h"
-
 ;
+
 /* Cria aleatoriamente um mapa */
 mapa::mapa(void)	{
 	//srand (time(NULL));
@@ -14,6 +14,21 @@ mapa::mapa(void)	{
 		p.y = rand() * 2000 / RAND_MAX;
 		arvores.push_front(* new posicao(p));
 	}
+	/* Colocar pedras */
+	for( int i=0 ; i < NUM_PEDRAS ; i++)	{
+		p.x = rand() * 2000 / RAND_MAX;
+		p.y = rand() * 2000 / RAND_MAX;
+		pedras.push_front(* new posicao(p));
+	}
+	for( int i=0 ; i < NUM_CHAVES ; i++)	{
+		p.x = rand() * 2000 / RAND_MAX;
+		p.y = rand() * 2000 / RAND_MAX;
+		chaves.push_front(* new posicao(p));
+	}
+	inicial.x = rand() *2000 / RAND_MAX;
+	inicial.y = rand() *2000 / RAND_MAX;
+	edificio.x = rand() *2000 / RAND_MAX;
+	edificio.y = rand() *2000 / RAND_MAX;
 }
 
 /* Cria um mapa com base num ficheiro de configuração * /
